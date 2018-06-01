bf0ef2ec4722e5bba7fcbcabf75a1c6332225408
Alexey Shakura <alexey.shakura@gmail.com>
2018-06-01 15:38:42 +0300

    Generating lib + app

---
+++ libs/mat-ngx-wig/src/mat-ngx-wig.module.spec.ts
@@ -0,0 +1,7 @@
+import { MatNgxWigModule } from './mat-ngx-wig.module';
+
+describe('MatNgxWigModule', () => {
+  it('should work', () => {
+    expect(new MatNgxWigModule()).toBeDefined();
+  });
+});
